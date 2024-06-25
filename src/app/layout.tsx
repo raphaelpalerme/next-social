9ad47aba251e762a2a3f5604204fc2abee7e038b
raphaelpalerme <raphaelpalerme2@gmail.com>
2024-06-25 21:57:04 +0400

    chore: add background color and padding to main content area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,12 +34,18 @@ export default function RootLayout({
       >
         <header
           className={cn(
-            "w-full bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64"
+            "w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64"
           )}
         >
           <Navbar />
         </header>
-        {children}
+        <div
+          className={cn(
+            "bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64"
+          )}
+        >
+          {children}
+        </div>
       </body>
     </html>
   );
